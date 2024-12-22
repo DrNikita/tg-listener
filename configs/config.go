298f2d375414ddebfddecb528205c1e99088ed67
Nikita 111 <hell.lol.world@gmail.com>
2024-12-22 18:41:56 +0300

    nothing

--- configs/config.go
+++ configs/config.go
@@ -1,9 +1,21 @@
 package configs
 
-type Config struct{}
+import (
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	TgToken string `envconfig:"TG_TOKEN"`
+}
 
 func MustConfig() (*Config, error) {
 	var config Config
+
+	err := envconfig.Process("", &config)
+	if err != nil {
+		return nil, err
+	}
 
 	return &config, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module tg-listener
 
 go 1.23.2
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/kelseyhightower/envconfig v1.4.0
+)
